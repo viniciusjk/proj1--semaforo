90c429d04cc6a09323a37fc96e2650917b07d069
viniciusjk <39393447+viniciusjk@users.noreply.github.com>
2018-06-12 21:11:07 -0300

    Add files via upload

---
+++ FLASH/Generated_Code/subdir.mk
@@ -0,0 +1,324 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+-include ../makefile.local
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS_QUOTED += \
+"../Generated_Code/AdcLdd1.c" \
+"../Generated_Code/BOTON.c" \
+"../Generated_Code/BitIoLdd1.c" \
+"../Generated_Code/BitIoLdd2.c" \
+"../Generated_Code/BitIoLdd3.c" \
+"../Generated_Code/BitIoLdd4.c" \
+"../Generated_Code/BitIoLdd5.c" \
+"../Generated_Code/Cpu.c" \
+"../Generated_Code/ExtIntLdd1.c" \
+"../Generated_Code/LDRConverter.c" \
+"../Generated_Code/LED_AMARELO.c" \
+"../Generated_Code/LED_VERDE.c" \
+"../Generated_Code/LED_VERDE_PEDESTRE.c" \
+"../Generated_Code/LED_VERMELHO.c" \
+"../Generated_Code/LED_VERMELHO_PEDESTRE.c" \
+"../Generated_Code/PE_LDD.c" \
+"../Generated_Code/TIMER2.c" \
+"../Generated_Code/TU1.c" \
+"../Generated_Code/TimerIntLdd1.c" \
+"../Generated_Code/Vectors.c" \
+
+C_SRCS += \
+../Generated_Code/AdcLdd1.c \
+../Generated_Code/BOTON.c \
+../Generated_Code/BitIoLdd1.c \
+../Generated_Code/BitIoLdd2.c \
+../Generated_Code/BitIoLdd3.c \
+../Generated_Code/BitIoLdd4.c \
+../Generated_Code/BitIoLdd5.c \
+../Generated_Code/Cpu.c \
+../Generated_Code/ExtIntLdd1.c \
+../Generated_Code/LDRConverter.c \
+../Generated_Code/LED_AMARELO.c \
+../Generated_Code/LED_VERDE.c \
+../Generated_Code/LED_VERDE_PEDESTRE.c \
+../Generated_Code/LED_VERMELHO.c \
+../Generated_Code/LED_VERMELHO_PEDESTRE.c \
+../Generated_Code/PE_LDD.c \
+../Generated_Code/TIMER2.c \
+../Generated_Code/TU1.c \
+../Generated_Code/TimerIntLdd1.c \
+../Generated_Code/Vectors.c \
+
+OBJS += \
+./Generated_Code/AdcLdd1.o \
+./Generated_Code/BOTON.o \
+./Generated_Code/BitIoLdd1.o \
+./Generated_Code/BitIoLdd2.o \
+./Generated_Code/BitIoLdd3.o \
+./Generated_Code/BitIoLdd4.o \
+./Generated_Code/BitIoLdd5.o \
+./Generated_Code/Cpu.o \
+./Generated_Code/ExtIntLdd1.o \
+./Generated_Code/LDRConverter.o \
+./Generated_Code/LED_AMARELO.o \
+./Generated_Code/LED_VERDE.o \
+./Generated_Code/LED_VERDE_PEDESTRE.o \
+./Generated_Code/LED_VERMELHO.o \
+./Generated_Code/LED_VERMELHO_PEDESTRE.o \
+./Generated_Code/PE_LDD.o \
+./Generated_Code/TIMER2.o \
+./Generated_Code/TU1.o \
+./Generated_Code/TimerIntLdd1.o \
+./Generated_Code/Vectors.o \
+
+C_DEPS += \
+./Generated_Code/AdcLdd1.d \
+./Generated_Code/BOTON.d \
+./Generated_Code/BitIoLdd1.d \
+./Generated_Code/BitIoLdd2.d \
+./Generated_Code/BitIoLdd3.d \
+./Generated_Code/BitIoLdd4.d \
+./Generated_Code/BitIoLdd5.d \
+./Generated_Code/Cpu.d \
+./Generated_Code/ExtIntLdd1.d \
+./Generated_Code/LDRConverter.d \
+./Generated_Code/LED_AMARELO.d \
+./Generated_Code/LED_VERDE.d \
+./Generated_Code/LED_VERDE_PEDESTRE.d \
+./Generated_Code/LED_VERMELHO.d \
+./Generated_Code/LED_VERMELHO_PEDESTRE.d \
+./Generated_Code/PE_LDD.d \
+./Generated_Code/TIMER2.d \
+./Generated_Code/TU1.d \
+./Generated_Code/TimerIntLdd1.d \
+./Generated_Code/Vectors.d \
+
+OBJS_QUOTED += \
+"./Generated_Code/AdcLdd1.o" \
+"./Generated_Code/BOTON.o" \
+"./Generated_Code/BitIoLdd1.o" \
+"./Generated_Code/BitIoLdd2.o" \
+"./Generated_Code/BitIoLdd3.o" \
+"./Generated_Code/BitIoLdd4.o" \
+"./Generated_Code/BitIoLdd5.o" \
+"./Generated_Code/Cpu.o" \
+"./Generated_Code/ExtIntLdd1.o" \
+"./Generated_Code/LDRConverter.o" \
+"./Generated_Code/LED_AMARELO.o" \
+"./Generated_Code/LED_VERDE.o" \
+"./Generated_Code/LED_VERDE_PEDESTRE.o" \
+"./Generated_Code/LED_VERMELHO.o" \
+"./Generated_Code/LED_VERMELHO_PEDESTRE.o" \
+"./Generated_Code/PE_LDD.o" \
+"./Generated_Code/TIMER2.o" \
+"./Generated_Code/TU1.o" \
+"./Generated_Code/TimerIntLdd1.o" \
+"./Generated_Code/Vectors.o" \
+
+C_DEPS_QUOTED += \
+"./Generated_Code/AdcLdd1.d" \
+"./Generated_Code/BOTON.d" \
+"./Generated_Code/BitIoLdd1.d" \
+"./Generated_Code/BitIoLdd2.d" \
+"./Generated_Code/BitIoLdd3.d" \
+"./Generated_Code/BitIoLdd4.d" \
+"./Generated_Code/BitIoLdd5.d" \
+"./Generated_Code/Cpu.d" \
+"./Generated_Code/ExtIntLdd1.d" \
+"./Generated_Code/LDRConverter.d" \
+"./Generated_Code/LED_AMARELO.d" \
+"./Generated_Code/LED_VERDE.d" \
+"./Generated_Code/LED_VERDE_PEDESTRE.d" \
+"./Generated_Code/LED_VERMELHO.d" \
+"./Generated_Code/LED_VERMELHO_PEDESTRE.d" \
+"./Generated_Code/PE_LDD.d" \
+"./Generated_Code/TIMER2.d" \
+"./Generated_Code/TU1.d" \
+"./Generated_Code/TimerIntLdd1.d" \
+"./Generated_Code/Vectors.d" \
+
+OBJS_OS_FORMAT += \
+./Generated_Code/AdcLdd1.o \
+./Generated_Code/BOTON.o \
+./Generated_Code/BitIoLdd1.o \
+./Generated_Code/BitIoLdd2.o \
+./Generated_Code/BitIoLdd3.o \
+./Generated_Code/BitIoLdd4.o \
+./Generated_Code/BitIoLdd5.o \
+./Generated_Code/Cpu.o \
+./Generated_Code/ExtIntLdd1.o \
+./Generated_Code/LDRConverter.o \
+./Generated_Code/LED_AMARELO.o \
+./Generated_Code/LED_VERDE.o \
+./Generated_Code/LED_VERDE_PEDESTRE.o \
+./Generated_Code/LED_VERMELHO.o \
+./Generated_Code/LED_VERMELHO_PEDESTRE.o \
+./Generated_Code/PE_LDD.o \
+./Generated_Code/TIMER2.o \
+./Generated_Code/TU1.o \
+./Generated_Code/TimerIntLdd1.o \
+./Generated_Code/Vectors.o \
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Generated_Code/AdcLdd1.o: ../Generated_Code/AdcLdd1.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #6 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/AdcLdd1.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/AdcLdd1.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/BOTON.o: ../Generated_Code/BOTON.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #7 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/BOTON.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/BOTON.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/BitIoLdd1.o: ../Generated_Code/BitIoLdd1.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #8 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/BitIoLdd1.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/BitIoLdd1.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/BitIoLdd2.o: ../Generated_Code/BitIoLdd2.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #9 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/BitIoLdd2.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/BitIoLdd2.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/BitIoLdd3.o: ../Generated_Code/BitIoLdd3.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #10 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/BitIoLdd3.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/BitIoLdd3.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/BitIoLdd4.o: ../Generated_Code/BitIoLdd4.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #11 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/BitIoLdd4.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/BitIoLdd4.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/BitIoLdd5.o: ../Generated_Code/BitIoLdd5.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #12 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/BitIoLdd5.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/BitIoLdd5.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/Cpu.o: ../Generated_Code/Cpu.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #13 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/Cpu.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/Cpu.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/ExtIntLdd1.o: ../Generated_Code/ExtIntLdd1.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #14 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/ExtIntLdd1.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/ExtIntLdd1.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/LDRConverter.o: ../Generated_Code/LDRConverter.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #15 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/LDRConverter.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/LDRConverter.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/LED_AMARELO.o: ../Generated_Code/LED_AMARELO.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #16 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/LED_AMARELO.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/LED_AMARELO.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/LED_VERDE.o: ../Generated_Code/LED_VERDE.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #17 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/LED_VERDE.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/LED_VERDE.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/LED_VERDE_PEDESTRE.o: ../Generated_Code/LED_VERDE_PEDESTRE.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #18 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/LED_VERDE_PEDESTRE.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/LED_VERDE_PEDESTRE.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/LED_VERMELHO.o: ../Generated_Code/LED_VERMELHO.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #19 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/LED_VERMELHO.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/LED_VERMELHO.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/LED_VERMELHO_PEDESTRE.o: ../Generated_Code/LED_VERMELHO_PEDESTRE.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #20 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/LED_VERMELHO_PEDESTRE.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/LED_VERMELHO_PEDESTRE.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/PE_LDD.o: ../Generated_Code/PE_LDD.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #21 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/PE_LDD.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/PE_LDD.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/TIMER2.o: ../Generated_Code/TIMER2.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #22 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/TIMER2.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/TIMER2.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/TU1.o: ../Generated_Code/TU1.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #23 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/TU1.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/TU1.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/TimerIntLdd1.o: ../Generated_Code/TimerIntLdd1.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #24 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/TimerIntLdd1.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/TimerIntLdd1.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+Generated_Code/Vectors.o: ../Generated_Code/Vectors.c
+	@echo 'Building file: $<'
+	@echo 'Executing target #25 $<'
+	@echo 'Invoking: ARM Ltd Windows GCC C Compiler'
+	"$(ARMSourceryDirEnv)/arm-none-eabi-gcc" "$<" @"Generated_Code/Vectors.args" -MMD -MP -MF"$(@:%.o=%.d)" -o"Generated_Code/Vectors.o"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
---
+++ FLASH/Sources/main.d
@@ -0,0 +1,163 @@
+Sources/main.o: ../Sources/main.c \
+ C:/Freescale/CW_MCU_v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix \
+ C:/Freescale/CW_MCU_v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix \
+ C:/Freescale/CW_MCU_v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/Cpu.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/PE_Types.h \
+ C:/Freescale/CW_MCU_v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h \
+ C:/Freescale/CW_MCU_v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h \
+ C:/Freescale/CW_MCU_v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h \
+ C:/Freescale/CW_MCU_v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h \
+ C:/Freescale/CW_MCU_v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h \
+ C:/Freescale/CW_MCU_v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint \
+ C:/Freescale/CW_MCU_v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h \
+ C:/Freescale/CW_MCU_v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/PE_Error.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/PE_Const.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/IO_Map.h \
+ C:/Freescale/CW_MCU_v10.6/MCU/ProcessorExpert/lib/Kinetis/iofiles/MKL25Z4.h \
+ ../Sources/Events.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/PE_Types.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/PE_Error.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/PE_Const.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/IO_Map.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/LED_VERMELHO.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/BitIoLdd1.h \
+ C:/Freescale/CW_MCU_v10.6/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/GPIO_PDD.h \
+ C:/Freescale/CW_MCU_v10.6/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/PDD_Types.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/Cpu.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/BitIoLdd1.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/TIMER2.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/TimerIntLdd1.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/TU1.h \
+ C:/Freescale/CW_MCU_v10.6/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/LPTMR_PDD.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/TimerIntLdd1.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/TU1.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/LED_AMARELO.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/BitIoLdd2.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/BitIoLdd2.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/LED_VERDE.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/BitIoLdd3.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/BitIoLdd3.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/LED_VERMELHO_PEDESTRE.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/BitIoLdd4.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/BitIoLdd4.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/LED_VERDE_PEDESTRE.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/BitIoLdd5.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/BitIoLdd5.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/BOTON.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/ExtIntLdd1.h \
+ C:/Freescale/CW_MCU_v10.6/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/PORT_PDD.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/ExtIntLdd1.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/LDRConverter.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/AdcLdd1.h \
+ C:/Freescale/CW_MCU_v10.6/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/ADC_PDD.h \
+ C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/AdcLdd1.h
+
+C:/Freescale/CW_MCU_v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/lib_ewl.prefix:
+
+C:/Freescale/CW_MCU_v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/common.prefix:
+
+C:/Freescale/CW_MCU_v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_c_version.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/Cpu.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/PE_Types.h:
+
+C:/Freescale/CW_MCU_v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/stdint.h:
+
+C:/Freescale/CW_MCU_v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ansi_parms.h:
+
+C:/Freescale/CW_MCU_v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewlGlobals.h:
+
+C:/Freescale/CW_MCU_v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/arm/ansi_prefix.ARM.h:
+
+C:/Freescale/CW_MCU_v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/os_enum.h:
+
+C:/Freescale/CW_MCU_v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/cstdint:
+
+C:/Freescale/CW_MCU_v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/limits_api.h:
+
+C:/Freescale/CW_MCU_v10.6/MCU/ARM_GCC_Support/ewl/EWL_C/include/ewl_lib_ext1.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/PE_Error.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/PE_Const.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/IO_Map.h:
+
+C:/Freescale/CW_MCU_v10.6/MCU/ProcessorExpert/lib/Kinetis/iofiles/MKL25Z4.h:
+
+../Sources/Events.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/PE_Types.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/PE_Error.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/PE_Const.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/IO_Map.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/LED_VERMELHO.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/BitIoLdd1.h:
+
+C:/Freescale/CW_MCU_v10.6/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/GPIO_PDD.h:
+
+C:/Freescale/CW_MCU_v10.6/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/PDD_Types.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/Cpu.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/BitIoLdd1.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/TIMER2.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/TimerIntLdd1.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/TU1.h:
+
+C:/Freescale/CW_MCU_v10.6/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/LPTMR_PDD.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/TimerIntLdd1.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/TU1.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/LED_AMARELO.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/BitIoLdd2.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/BitIoLdd2.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/LED_VERDE.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/BitIoLdd3.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/BitIoLdd3.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/LED_VERMELHO_PEDESTRE.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/BitIoLdd4.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/BitIoLdd4.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/LED_VERDE_PEDESTRE.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/BitIoLdd5.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/BitIoLdd5.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/BOTON.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/ExtIntLdd1.h:
+
+C:/Freescale/CW_MCU_v10.6/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/PORT_PDD.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/ExtIntLdd1.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/LDRConverter.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/AdcLdd1.h:
+
+C:/Freescale/CW_MCU_v10.6/MCU/ProcessorExpert/lib/Kinetis/pdd/inc/ADC_PDD.h:
+
+C:/Users/ea076/Desktop/Grupo\ -\ Caio\ e\ Jojo/Semaforo/Generated_Code/AdcLdd1.h:
